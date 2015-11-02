8ddf823a747e2ac4ff84404033f07f1a9ef389b8
evoskuil <eric@voskuil.org>
2015-11-01 18:58:18 -0800

    Generalize test seed nodes.

--- test/network/network.cpp
+++ test/network/network.cpp
@@ -27,10 +27,17 @@
 using namespace bc;
 using namespace bc::network;
 
-#define TEST_SET_NAME "network_tests"
+#define TEST_SET_NAME \
+    "network_tests"
 
 #define TEST_NAME \
     boost::unit_test::framework::current_test_case().p_name
+
+#define SEED1 \
+    { "testnet-seed.bluematt.me:18333" }
+
+#define SEED1_AUTHORITY \
+    { "[2604:a880:1:20::269:c001]:18333" }
 
 #define SETTINGS_TESTNET_ONE_THREAD_NO_CONNECTIONS(config) \
     settings config = p2p::testnet; \
@@ -42,7 +49,7 @@ using namespace bc::network;
 #define SETTINGS_TESTNET_ONE_THREAD_ONE_SEED(config) \
     SETTINGS_TESTNET_ONE_THREAD_NO_CONNECTIONS(config); \
     config.host_pool_capacity = 42; \
-    configuration.seeds = { configuration.seeds[1] }; \
+    configuration.seeds = { SEED1 }; \
     configuration.hosts_file = get_log_path(TEST_NAME, "hosts")
 
 std::string get_log_path(const std::string& test, const std::string& file)
@@ -103,6 +110,28 @@ static int stop_result(p2p& network)
 
 BOOST_FIXTURE_TEST_SUITE(network_tests, log_setup_fixture)
 
+BOOST_AUTO_TEST_CASE(p2p__height__default__zero)
+{
+    p2p network;
+    BOOST_REQUIRE_EQUAL(network.height(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(p2p__set_height__value__expected)
+{
+    p2p network;
+    const size_t expected_height = 42;
+    network.set_height(expected_height);
+    BOOST_REQUIRE_EQUAL(network.height(), expected_height);
+}
+
+BOOST_AUTO_TEST_CASE(p2p__start__no_sessions__start_success__success)
+{
+    print_headers(TEST_NAME);
+    SETTINGS_TESTNET_ONE_THREAD_NO_CONNECTIONS(configuration);
+    p2p network(configuration);
+    BOOST_REQUIRE_EQUAL(start_result(network), error::success);
+}
+
 BOOST_AUTO_TEST_CASE(p2p__start__no_connections__start_stop_success)
 {
     print_headers(TEST_NAME);
@@ -128,6 +157,16 @@ BOOST_AUTO_TEST_CASE(p2p__start__seed_session__start_stop_start_success)
     p2p network(configuration);
     BOOST_REQUIRE_EQUAL(start_result(network), error::success);
     BOOST_REQUIRE_EQUAL(stop_result(network), error::success);
+    BOOST_REQUIRE_EQUAL(start_result(network), error::success);
+}
+
+BOOST_AUTO_TEST_CASE(p2p__start__seed_session__start_stop1_start_success)
+{
+    print_headers(TEST_NAME);
+    SETTINGS_TESTNET_ONE_THREAD_ONE_SEED(configuration);
+    p2p network(configuration);
+    BOOST_REQUIRE_EQUAL(start_result(network), error::success);
+    network.stop();
     BOOST_REQUIRE_EQUAL(start_result(network), error::success);
 }
 
@@ -193,17 +232,11 @@ BOOST_AUTO_TEST_CASE(p2p__start__seed_session_blacklisted__start_operation_faile
     SETTINGS_TESTNET_ONE_THREAD_NO_CONNECTIONS(configuration);
     configuration.host_pool_capacity = 42;
     configuration.hosts_file = get_log_path(TEST_NAME, "hosts");
-    configuration.seeds =
-    {
-        { "testnet-seed.bluematt.me:18333" }
-    };
-    configuration.blacklists =
-    {
-        { "[2604:a880:1:20::269:c001]:18333" }
-    };
+    configuration.seeds = { SEED1 };
+    configuration.blacklists = { SEED1_AUTHORITY };
     p2p network(configuration);
 
-    // The blacklist may not be complete, in which case this can fail.
+    // The blacklist may not be complete, in which case this test can fail.
     BOOST_CHECK_EQUAL(start_result(network), error::operation_failed);
     BOOST_REQUIRE_EQUAL(stop_result(network), error::success);
 }
